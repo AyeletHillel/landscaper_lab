812c5da664a87dc127855cc1da2d264a6a2f9353
Ayelet Hillel <ah3708@tc.columbia.edu>
2023-02-01 14:20:18 -0500

    game completed

--- landscaper.py
+++ landscaper.py
@@ -1,9 +1,3 @@
-# while(True):
-#     user_choice = input("[1] Mow Lawn [2] Check Stats [Q] Quit")
-    
-#     if(user_choice == "Q"):
-#         break
-
 game = {"tool": 0, "money": 0}
 
 tools = [
@@ -26,7 +20,49 @@ def check_stats():
     tool = tools[game["tool"]]
     print(f"You currently have ${game['money']} and are using {tool['name']}")
 
-check_stats()
-cut_lawns()
+def upgrate():
+    if (game["tool"] >= (len(tools) -1)):
+        print("No more upgrades")
+        return 0
+    next_tool = tools[game["tool"] + 1]
+    if (next_tool == None):
+        print("There is no more tools")
+        return 0
+    if (game["money"] < next_tool["cost"]):
+        print("not enough money to buy this tool")
+        return 0
+    print("You are upgrading your tool")
+    # update game money
+    game["money"] -= next_tool["cost"]
+    # update game tool
+    game["tool"] += 1
+
+def win_check():
+    if (game["tool"] == 4 and game["money"] >= 1000):
+        print("You win")
+        return True
+    return False
+
+while(True):
+    
+    i = input("[1] Cut Lawn [2] Check Stats [3] Upgrate [4] Quit")
+    
+    i = int(i)
+    
+    if (i == 1):
+        cut_lawns()
+    
+    if (i == 2):
+        check_stats()
+    
+    if (i == 3):
+        upgrate()
+    
+    if (i == 4):
+        print("You quit the game")
+        break
+    if (win_check()):
+        break
+
 
 
